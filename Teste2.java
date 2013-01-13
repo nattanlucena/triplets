5a246cec0bcef7556b97d956f8284941595e2f53
Nattan Lucena <nattanlucena@gmail.com>
2013-01-13 18:16:05 -0300

    v 0.2

--- Teste2.java
+++ Teste2.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 
@@ -5,49 +8,29 @@ public class Teste2 {
 
 	public Teste2(){
 		int t;
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in).useDelimiter(" ");
 		
 		try {
 			t = sc.nextInt();
-			int[] seq = new int[t];
-			
-			if ( (t > 0) && (t <= Math.pow(10, 5)) ){
+			ArrayList<Integer> array1 = new ArrayList<Integer>();
 				for (int i =0; i< t; i++){
 					
-					if(sc.hasNextInt()){
-						if((sc.nextInt() < 0) || sc.nextInt()  > (Math.pow(2, 31) -1)  ){
-							break;
-						}else{
-							if( (i == 0) ){
-								seq[i] = sc.nextInt();
-							}else{
-								if((seq[i] > seq[i -1])){
-									if(i >=2 ){
-										if( seq[i] == seq[i-2]){
-											break;
-										}else{
-											seq[i] = sc.nextInt();
-										}
-									}
-								}else{
-									break;
-								}
-							}
+					while(sc.hasNextInt()){
+							int x = sc.nextInt();
+							if(!array1.contains(x)){	
+								array1.add(x);		
 						}
 					}
-					
-					System.out.println(seq[i]);
+				
+					Collections.sort(array1);
 				}
-			}
-			
-			/* um for pra gerar uma string do seq -> S{n, n+1, n+2} e adicionar numa lista, verificando se já existe utilizando o método contains()
-			 * https://www.interviewstreet.com/challenges/dashboard/#problem/50bff669014b8
-			 */
+				
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
 	
 	
 	public static void main(String[] args) {
